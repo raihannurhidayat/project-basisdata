1b54829332294e30449a144cb7de1abee4e0deb3
severusDude <fajarsaladin05@gmail.com>
2024-05-19 21:42:31 +0700

    feat(endpoint): add user register, login and logout

--- server/main/api/urls.py
+++ server/main/api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
-from .views import user_list, user_detail, category_list, category_detail, thread_list, thread_detail, post_list, post_detail
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
 
 urlpatterns = [
-    path('users/', user_list, name='user_list'),
-    path('users/<int:pk>/', user_detail, name='user_detail'),
+    path('auth/register', views.register_user, name='register'),
+    path('auth/login', views.login, name='login'),
+    path('auth/logout', views.logout, name='logout'),
 
-    path('categories/', category_list, name='category_list'),
-    path('categories/<int:pk>/', category_detail, name='category_detail'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/<int:pk>/', views.user_detail, name='user_detail'),
 
-    path('threads/', thread_list, name='thread_list'),
-    path('threads/<int:pk>/', thread_detail, name='thread_detail'),
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
 
-    path('posts/', post_list, name='post_list'),
-    path('posts/<int:pk>/', post_detail, name='post_detail'),
+    path('threads/', views.thread_list, name='thread_list'),
+    path('threads/<int:pk>/', views.thread_detail, name='thread_detail'),
+
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
 ]
