1d57fbbad71b452b21a8b56f080752c82e55ca64
severusDude <fajarsaladin05@gmail.com>
2024-05-20 20:46:26 +0700

    build(db): finalize custom user model

--- server/main/api/models.py
+++ server/main/api/models.py
@@ -1,34 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
-# User Manager
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-
-        if not email:
-            raise ValueError('The Email field must be set')
-
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, email, password, **extra_fields)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from . import managers
 
 # Create your models here.
 
 
-class User(models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
@@ -36,8 +15,11 @@ class User(models.Model):
     profile_picture_url = models.TextField(blank=True, null=True)
     user_bio = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
-    objects = UserManager()
+    objects = managers.UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', )
