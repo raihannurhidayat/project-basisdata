0c57be39516f76b20093b347b4aa76489a3d3041
severusDude <fajarsaladin05@gmail.com>
2024-05-23 11:03:17 +0700

    feat(settings): add default pagination configuration

--- server/main/main/settings.py
+++ server/main/main/settings.py
@@ -70,6 +70,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # Allow all origins for now
