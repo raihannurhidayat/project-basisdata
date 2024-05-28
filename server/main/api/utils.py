d9049126534cbe0332ddf49387ce57b62a6d9f83
severusDude <fajarsaladin05@gmail.com>
2024-05-28 21:48:10 +0700

    ci(pagination): set default pagination as 10

--- server/main/api/utils.py
+++ server/main/api/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    # page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
+    page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
     page_query_param = 'page'
-    page_size_query_param = 'page_size'
+    # page_size_query_param = 'page_size'
     max_page_size = 100
