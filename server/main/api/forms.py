72c5999707a07a48704e61ffdab705cb3895e5db
severusDude <fajarsaladin05@gmail.com>
2024-05-28 19:24:29 +0700

    fix(forms): field not found

--- server/main/api/forms.py
+++ server/main/api/forms.py
@@ -6,10 +6,10 @@ from .models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'username', 'profile_picture')
+        fields = ('email',)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'username', 'profile_picture')
+        fields = ('email',)
